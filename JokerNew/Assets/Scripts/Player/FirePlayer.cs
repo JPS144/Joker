d187feadda87cefe9b0a2d0fc2ef2a774958fe5b
Jordan Sisco <jordanpsisco@gmail.com>
2015-05-06 18:46:26 -0700

    5/6/15
    Updated Audio

--- JokerNew/Assets/Scripts/Player/FirePlayer.cs
+++ JokerNew/Assets/Scripts/Player/FirePlayer.cs
@@ -4,14 +4,19 @@ using System.Collections;
 public class FirePlayer : MonoBehaviour {
     // Bullet Prefab
     public GameObject bullet;
+	public Animation BulletUp;
+	public Animation BulletDown;
 
+
+	
     // Update is called once per frame
     void Update () {
-        if (Input.GetKeyDown(KeyCode.Space)) {
-            // Spawn the Bullet
-            GameObject g = (GameObject) Instantiate(bullet, transform.position,Quaternion.identity);
-            // Ignore Bullet<->Player collisions
-            Physics2D.IgnoreCollision(g.GetComponent<Collider2D>(), transform.parent.GetComponent<Collider2D>());
-        }
-    }
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			// Spawn the Bullet
+			bullet.transform.rotation = gameObject.transform.rotation;
+			GameObject g = (GameObject)Instantiate (bullet, transform.position, Quaternion.identity);
+			// Ignore Bullet<->Player collisions
+			Physics2D.IgnoreCollision (g.GetComponent<Collider2D> (), transform.parent.GetComponent<Collider2D> ());
+		}
+	}
 }
---
+++ JokerNew/Assets/Scripts/Player/Test/Shooting.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shooting : MonoBehaviour {
+
+	public GameObject ThePrefab;
+	private float fireRate = .15f;
+	private float nextFire = 0;
+	private float bulletSpeed = 500f;
+	
+	void Update () {
+		GameObject instance;
+		
+		//shoot up
+		if (Input.GetKeyDown(KeyCode.W) && Time.time > nextFire){
+			nextFire = Time.time + fireRate;
+			Debug.Log ("nf" + nextFire);
+			instance = Instantiate(ThePrefab, transform.position, transform.rotation) as GameObject;
+			instance.GetComponent<Rigidbody2D>().AddForce(transform.up * bulletSpeed);
+		}
+		
+		//shoot left
+		if (Input.GetKeyDown(KeyCode.A) && Time.time > nextFire){
+			nextFire = Time.time + fireRate;
+			instance = Instantiate(ThePrefab, transform.position, transform.rotation) as GameObject;
+			instance.transform.rotation = Quaternion.AngleAxis(0, -Vector2.right);
+			instance.GetComponent<Rigidbody2D>().AddForce(-transform.right * -bulletSpeed);
+		}
+		
+		//shoot down
+		if (Input.GetKeyDown(KeyCode.S) && Time.time > nextFire){
+			nextFire = Time.time + fireRate;
+			instance = Instantiate(ThePrefab, transform.position, transform.rotation) as GameObject;
+			instance.transform.rotation = Quaternion.AngleAxis(0, -Vector2.up);
+			instance.GetComponent<Rigidbody2D>().AddForce(-transform.up * -bulletSpeed);
+		}
+		
+		//shoot right
+		if (Input.GetKeyDown(KeyCode.D) && Time.time > nextFire){
+			nextFire = Time.time + fireRate;
+			instance = Instantiate(ThePrefab, transform.position, transform.rotation) as GameObject;
+			instance.transform.rotation = Quaternion.AngleAxis(0, Vector2.right);
+			instance.GetComponent<Rigidbody2D>().AddForce(transform.right * bulletSpeed);
+		}
+	}
+}
